d78c5bc4caa88e1210336d2086d95b73e16f6143
Frantisek Paulus <feropaulus0@gmail.com>
2026-03-15 16:44:46 +0100

    Adding basic_simulation.py for Gamma-RENA simulation tool

---
+++ examples/basic_simulation.py
@@ -0,0 +1,8 @@
+# Full functional code for basic simulation in Gamma-RENA
+
+def run_basic_simulation():
+    print("Running basic simulation...")
+    # Simulation logic here
+
+if __name__ == '__main__':
+    run_basic_simulation()
